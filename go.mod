f80a5c6a0cf270c582baf0ed585c4c2e76462d9f
Louis Royer <infos.louis.royer@gmail.com>
2024-06-04 14:51:31 +0200

    wip

--- go.mod
+++ go.mod
@@ -7,24 +7,25 @@ toolchain go1.21.8
 require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/gofrs/uuid v4.4.0+incompatible
-	github.com/nextmn/go-pfcp-networking v0.0.27
+	github.com/nextmn/go-pfcp-networking v0.0.30
 	github.com/nextmn/json-api v0.0.2
 	github.com/urfave/cli/v2 v2.27.2
+	github.com/wmnsk/go-pfcp v0.0.24
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/bytedance/sonic v1.11.6 // indirect
+	github.com/bytedance/sonic v1.11.8 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.20.0 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/go-playground/validator/v10 v10.21.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -35,8 +36,7 @@ require (
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
-	github.com/wmnsk/go-pfcp v0.0.24 // indirect
-	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
--- runtime/upf.go
+++ runtime/upf.go
@@ -9,6 +9,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	pfcp_networking "github.com/nextmn/go-pfcp-networking/pfcp"
+	pfcputil "github.com/nextmn/go-pfcp-networking/pfcputil"
+	"github.com/wmnsk/go-pfcp/ie"
+	"github.com/wmnsk/go-pfcp/message"
 )
 
 var Ctrl *CtrlConfig
@@ -36,14 +39,65 @@ func Run() error {
 	return nil
 }
 
+func pushRTRRule(ue_ip string, gnb_ip string, teid_downlink uint32) {
+}
+
+func updateRoutersRules(msgType pfcputil.MessageType, message pfcp_networking.ReceivedMessage, e *pfcp_networking.PFCPEntityUP) {
+	for _, session := range e.GetPFCPSessions() {
+		session.RLock()
+		defer session.RUnlock()
+		for _, pdrid := range session.GetSortedPDRIDs() {
+			pdr, err := session.GetPDR(pdrid)
+			if err != nil {
+				continue
+			}
+			farid, err := pdr.FARID()
+			if err != nil {
+				continue
+			}
+			if source_iface, err := pdr.SourceInterface(); (err != nil) || (source_iface != ie.SrcInterfaceCore) {
+				continue
+			}
+			ue_ip_addr, err := pdr.UEIPAddress()
+			if err != nil {
+				continue
+			}
+
+			// FIXME: temporary hack, no IPv6 support
+			ue_ipv4 := ue_ip_addr.IPv4Address.String()
+
+			far, err := session.GetFAR(farid)
+			if err != nil {
+				continue
+			}
+			ForwardingParametersIe := far.ForwardingParameters()
+			if ohc, err := ForwardingParametersIe.OuterHeaderCreation(); err == nil {
+				// FIXME: temporary hack, no IPv6 support
+				gnb_ipv4 := ohc.IPv4Address.String()
+				teid_downlink := ohc.TEID
+				go pushRTRRule(ue_ipv4, gnb_ipv4, teid_downlink)
+			} else {
+				continue
+			}
+		}
+	}
+}
+
 func createPFCPNode() error {
 	if Ctrl.PFCPAddress == nil {
 		return fmt.Errorf("Missing pfcp address")
 	}
 	PFCPServer = pfcp_networking.NewPFCPEntityUP(*Ctrl.PFCPAddress)
-	pdu_modif_req, err := PFCPServer.GetHandler[]
-	pdu_estab_req, err := PFCPServer.GetHandler[]
-	PFCPServer.AddHandlers(map[pfcputil.MessageType)pfcp_networking.Handler
+	PFCPServer.AddHandler(message.MsgTypeSessionEstablishmentRequest, func(msg pfcp_networking.ReceivedMessage) error {
+		err := pfcp_networking.DefaultSessionEstablishmentRequestHandler(msg)
+		go updateRoutersRules(message.MsgTypeSessionEstablishmentRequest, msg, PFCPServer)
+		return err
+	})
+	PFCPServer.AddHandler(message.MsgTypeSessionModificationRequest, func(msg pfcp_networking.ReceivedMessage) error {
+		err := pfcp_networking.DefaultSessionModificationRequestHandler(msg)
+		go updateRoutersRules(message.MsgTypeSessionModificationRequest, msg, PFCPServer)
+		return err
+	})
 
 	PFCPServer.Start()
 	return nil
